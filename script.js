c9cc368443a37996569514c41f8dcd02f2c516ea
sorenado <142324524+sorenado@users.noreply.github.com>
2023-10-08 15:39:24 -0500

    I actually made the countdown

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-count = 10;
-const countingDown = () => {
-    document.getElementById("countdown").style.display("block");
-    let count = document.getElementByID("countdown").innerHTML;
-    count -= count;
+let setTimer;
+const actualCount = document.getElementById("countdown");
+let count = 1;
+function startTim(){
+  /*when button pressed, check if timer is running*/
+  if(!setTimer){
+    count = parseInt(document.getElementById("countN").value.trim());
+    if(count > 0){
+      actualCount.innerHTML = count;
+      actualCount.style.display = "block";
+      if(!isNaN(count)){
+        setTimer = setInterval(countingDown, 1000);
+      }
+    }
+  }
+  
+}
+
+function countingDown(){
+  count -= 1;
+  actualCount.innerHTML = count;
+  
+  if(count === 0){
+      clearInterval(setTimer);
+      setTimer = null;
+      count = 10;
+    }
 }
