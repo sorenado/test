29a8860afb6d81d4649be78154bc5257a89bd2f9
sorenado <142324524+sorenado@users.noreply.github.com>
2023-10-10 14:17:13 -0500

    The table and menu basic functionality is working!

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menu = document.getElementById("menu");
 const menuButton = document.getElementById("topbutton");
 menuButton.addEventListener("click", () => {
+    menu.style.display = "block";
     menu.classList.toggle("act");
 });
