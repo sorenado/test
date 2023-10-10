0f6b869d1b0276b08f00abbbadd8704e960bb3e1
sorenado <142324524+sorenado@users.noreply.github.com>
2023-10-10 14:13:57 -0500

    ttying to toggle act class in hopes that menu will slide to the right.
    This is a lot man.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menu = document.getElementById("menu");
 const menuButton = document.getElementById("topbutton");
 menuButton.addEventListener("click", () => {
+    menu.classList.toggle("act");
 });
