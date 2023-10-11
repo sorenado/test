1e114744a475b269ca6a4f4ca938c4ff76fdb5a9
sorenado <142324524+sorenado@users.noreply.github.com>
2023-10-11 00:35:37 -0500

    created first heading text that fades in

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menu = document.getElementById("menu");
     const menuButton = document.getElementById("topbutton");
+    
+
     menuButton.addEventListener("click", () => {
         menu.classList.toggle("act");
         menu.style.display = "block";
     });
+    
+
 });
