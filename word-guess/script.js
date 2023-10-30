c4922545e92880a6b120df9d680737847b2d0519
sorenado <142324524+sorenado@users.noreply.github.com>
2023-10-29 22:21:54 -0500

    set up basics for js. Not sure it will work though

---
+++ word-guess/script.js
@@ -0,0 +1,24 @@
+const words = ["sus", "amongus", "imposter"];
+const userGuess = document.getElementById("user-guess").value;
+lowerGuess = userGuess.toLowerCase();
+const correct = document.getElementById("correct");
+const incorrect = document.getElementById("incorrect");
+
+function guessNumber(){
+    for(let i = 0; i < words.length; i++){
+        if(lowerGuess === words[i]){
+            return true;
+            break;
+        }
+    }
+}
+
+function checkForTrue(){
+    if(guessNumber()){
+        correct.style.display = "block";
+        incorrect.style.display = "none";
+    }else{
+        incorrect.style.display = "block";
+        correct.style.display = "none";
+    }
+}
