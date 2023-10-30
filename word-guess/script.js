84f593248eee32e8e1ac2c96f014265aad0b6d91
sorenado <142324524+sorenado@users.noreply.github.com>
2023-10-30 03:24:20 -0500

    try it

--- word-guess/script.js
+++ word-guess/script.js
@@ -1,11 +1,11 @@
 const words = ["sus", "amongus", "imposter"];
-const userGuess = document.getElementById("user-guess").value;
-const lowerGuess = userGuess.toLowerCase();
 const correct = document.getElementById("correct");
 const incorrect = document.getElementById("incorrect");
 const checkButton = document.getElementById("check");
+const guessBox = document.getElementById("user-guess")
 
-function guessNumber(){
+function guessNumber(userGuess){
+    const lowerGuess = userGuess.toLowerCase();
     for(let i = 0; i < words.length; i++){
         if(lowerGuess === words[i]){
             return true;
@@ -15,21 +15,23 @@ function guessNumber(){
 }
 
 function checkForTrue(){
-    if(guessNumber() === true){
+    const userGuess = document.getElementById("user-guess").value;
+    if(guessNumber(userGuess) === true){
         correct.style.display = "block";
         incorrect.style.display = "none";
     }else{
         incorrect.style.display = "block";
         correct.style.display = "none";
+        userGuess = "";
     }
 }
 
 function checkUserRetry(){
-    if(userGuess !=="" && incorrect.style.display === "block"){
+    if(guessBox.value !=="" && incorrect.style.display === "block"){
         incorrect.style.display = "none";
     }
 }
 
-checkButton.addEventListener("click", checkForTrue());
-checkUserRetry();
+checkButton.addEventListener("click", checkForTrue);
+guessBox.addEventListener("keypress", checkUserRetry);
 
