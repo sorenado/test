0caac93014c3407933423bffdb71bb71c193e047
sorenado <142324524+sorenado@users.noreply.github.com>
2023-10-29 22:32:05 -0500

    basics have been done. I don't know if they work

--- word-guess/script.js
+++ word-guess/script.js
@@ -3,6 +3,7 @@ const userGuess = document.getElementById("user-guess").value;
 lowerGuess = userGuess.toLowerCase();
 const correct = document.getElementById("correct");
 const incorrect = document.getElementById("incorrect");
+checkButton = document.getElementById("check");
 
 function guessNumber(){
     for(let i = 0; i < words.length; i++){
@@ -22,3 +23,13 @@ function checkForTrue(){
         correct.style.display = "none";
     }
 }
+
+function checkUserRetry(){
+    if(userGuess !=="" && incorrect.style.display === "block"){
+        incorrect.style.display = "none";
+    }
+}
+
+checkButton.addEventListener("click", checkForTrue());
+checkUserRetry();
+
