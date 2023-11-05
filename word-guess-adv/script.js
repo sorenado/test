2fc14ced9cbd5acc77affb4d5017333d92d85d32
sorenado <142324524+sorenado@users.noreply.github.com>
2023-11-05 01:37:49 -0500

    the basics have been achieved.

--- word-guess-adv/script.js
+++ word-guess-adv/script.js
@@ -6,57 +6,56 @@ const guessBox = document.getElementById("user-guess");
 const hangmanContent = document.getElementById("hangman-content");
 let correctWord = words[Math.floor(Math.random() * words.length)]; // Random number from 1 to words length 
 console.log(correctWord);
-let bob;
 
 
 
-let spacedWords = [];
-// Spaces the words so each individual character can be checked
+
+let spaced;
+// Spaces the word so each individual character can be checked
 function seperateWords(words) {
-  for (let i = 0; i < words.length; i++) {
-    let spaced = words[i].split('');
-    spacedWords.push(spaced);
-
-  };
+  spaced = words.split('');
+  return spaced;
 };
 
 
-
+let hangmanBarsMerge;
 let hangmanBars = [];
 function generateHangmanBars(word) {
-  for(let i = 0; i < words.length;i++){
+  for (let i = 0; i < word.length; i++) {
     hangmanBars[i] = "-";
   }
-  bob = hangmanBars.join('');
+  hangmanBarsMerge = hangmanBars.join('');
 };
 
 
 function displayBars() {
-  let cont = `<p>${bob}</p>`;
+  let cont = `<p>${hangmanBarsMerge}</p>`;
   hangmanContent.innerHTML = cont;
 }
 
 function checkForTrue() {
   const userGuess = document.getElementById("user-guess").value;
-  if (guessNumber(userGuess) === true) {
-    correct.style.display = "block";
-    incorrect.style.display = "none";
-  } else {
-    incorrect.style.display = "block";
-    correct.style.display = "none";
-    guessBox.value = "";
+  console.log("working!");
+  if (correctWord.includes(userGuess) && userGuess !== "") {
+    console.log("it is true.");
+    let hangmanSplit = hangmanBarsMerge.split("");
+    console.log(hangmanSplit);
+    for(let i = 0; i < correctWord.length; i++){
+      if (seperateWords(correctWord)[i] === userGuess){
+        hangmanSplit[i] = userGuess;
+      } else {
+        console.log("bad");
+      
+      }
+    }
+    console.log(hangmanSplit);
+    hangmanContent.innerHTML = hangmanSplit.join('');
+    hangmanBarsMerge = hangmanSplit.join('');
   }
 }
 
 function checkUserRetry() {
-  if (guessBox.value !== "" && incorrect.style.display === "block") {
-    incorrect.style.display = "none";
-  }
-}
-
-function checkEnterPress(e) {
-  if (e.key === "Enter" && guessBox.value !== "" || guessBox.value !== " ") {
-    checkForTrue();
+  if (guessBox.value !== "") {
   }
 }
 
@@ -66,6 +65,3 @@ generateHangmanBars(correctWord);
 displayBars();
 
 checkButton.addEventListener("click", checkForTrue);
-guessBox.addEventListener("keypress", checkUserRetry);
-guessBox.addEventListener("keypress", checkEnterPress);
-
